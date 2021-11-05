956600dbe4f840b32542594971695cc78459b510
ymrl <ymrl@users.noreply.github.com>
2021-11-05 08:41:27 +0000

    deploy: 5cdad4c96b03fae71680325cab2f943618eab907

---
+++ _next/static/lChHewSfLSjd6h7HdB1hh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/252f366e-25f31331ea28beeeb530.js","static/css/5178682ba1db5e60efad.css","static/chunks/pages/index-063c1375a5f410ad7a11.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
