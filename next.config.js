154dc07b9e1a611b7d903236a97c4e566c0db492
ymrl <ymrl@freee.co.jp>
2023-01-20 18:03:45 +0900

    :up: update mdx-loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require('@next/mdx')();
+const withMDX = require('@next/mdx')({
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: '@mdx-js/react',
+  },
+});
 
 const urlPrefix = process.env.URL_PREFIX ? `/${process.env.URL_PREFIX}` : '';
 
