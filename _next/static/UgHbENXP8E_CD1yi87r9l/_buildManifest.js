89c357b943928679a1d3331e76c262dc4665b170
ymrl <ymrl@users.noreply.github.com>
2023-04-06 02:33:53 +0000

    deploy: 33b4a845caa7f72a0f15fc938dda080b794b836e

--- _next/static/UgHbENXP8E_CD1yi87r9l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8de3caa4cc9a3c4b.js"],"/_error":["static/chunks/pages/_error-28b0dba9dbcfb4ed.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
