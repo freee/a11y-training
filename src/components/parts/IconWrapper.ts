7869c12b83d5359a9e4bf30a550268a88b87cfa7
Takuya Nishimoto <nishimotz@gmail.com>
2023-04-10 11:39:52 +0900

    ID 0471,0531,0561

---
+++ src/components/parts/IconWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const IconWrapper = styled.span`
+  display: inline-flex;
+  align-items: center;
+  width: 1.5rem;
+  height: 1.5rem;
+  font-size: 1.5rem;
+  margin-right: 0.5rem;
+  color: blue;
+`;
