0f06d1a16d29c03cf516a4d4baef6dc7b5a47f19
Masafumi NAKANE <max@freee.co.jp>
2023-04-14 17:26:19 +0900

    Use Ul, Li, TextLink, H2, H3, H4, and H5 instead.

--- src/components/parts/RelatedChecks.tsx
+++ src/components/parts/RelatedChecks.tsx
@@ -1,5 +1,12 @@
 // RelatedChecks.tsx
 import React from 'react';
+import { Ul } from './Ul';
+import { Li } from './Li';
+import { TextLink } from './TextLink';
+import { H2 } from './H2';
+import { H3 } from './H3';
+import { H4 } from './H4';
+import { H5 } from './H5';
 
 type RelatedChecksProps = {
   ids: string[];
@@ -14,18 +21,31 @@ const RelatedChecks: React.FC<RelatedChecksProps> = ({
   headerLevel = 4,
   baseURL = 'https://a11y-guidelines.freee.co.jp/checks/checklist.html#check-',
 }) => {
-  const HeaderTag = `h${headerLevel}` as keyof JSX.IntrinsicElements;
-
+  const renderHeader = () => {
+    const HeaderTag = `h${headerLevel}` as keyof JSX.IntrinsicElements;
+    switch (headerLevel) {
+      case 2:
+        return <H2>{headerText}</H2>;
+      case 3:
+        return <H3>{headerText}</H3>;
+      case 4:
+        return <H4>{headerText}</H4>;
+      case 5:
+        return <H5>{headerText}</H5>;
+      default:
+        return <HeaderTag>{headerText}</HeaderTag>;
+    }
+  };
   return (
     <div>
-      <HeaderTag>{headerText}</HeaderTag>
-      <ul>
+      {renderHeader()}
+      <Ul>
         {ids.map((id) => (
-          <li key={id}>
-            <a href={`${baseURL}${id}`}>チェックID：{id}</a>
-          </li>
+          <Li key={id}>
+            <TextLink href={`${baseURL}${id}`}>チェックID：{id}</TextLink>
+          </Li>
         ))}
-      </ul>
+      </Ul>
     </div>
   );
 };
