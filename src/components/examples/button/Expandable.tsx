afb3105a73a5a6203a826351acb6a423e74f4d6a
ymrl <ymrl@users.noreply.github.com>
2023-06-06 08:36:58 +0000

    Apply Prettier Change

--- src/components/examples/button/Expandable.tsx
+++ src/components/examples/button/Expandable.tsx
@@ -4,26 +4,26 @@ import styled from 'styled-components';
 import { Button } from '../../parts/Button';
 
 export const HamburgerStyle = styled.div`
-    height: 12.5rem;
-    .hamburgermenu {
-        background: #ffffff;
-        overflow: hidden;
-        white-space: nowrap;
-        transition: width 0.5s;
-    }
-    // ハンバーガーメニューが閉じているときには幅を0にする
-    .hamburgermenu:not(.expanded) {
-        width: 0;
-    }
-    // ハンバーガーメニューが開いたときに幅を指定する。アニメーションでせり出すように見える
-    .hamburgermenu.expanded {
-        width: 12.5rem;
-    }
+  height: 12.5rem;
+  .hamburgermenu {
+    background: #ffffff;
+    overflow: hidden;
+    white-space: nowrap;
+    transition: width 0.5s;
+  }
+  // ハンバーガーメニューが閉じているときには幅を0にする
+  .hamburgermenu:not(.expanded) {
+    width: 0;
+  }
+  // ハンバーガーメニューが開いたときに幅を指定する。アニメーションでせり出すように見える
+  .hamburgermenu.expanded {
+    width: 12.5rem;
+  }
 `;
 
 const NavList = styled.ul`
-    list-style: none;
-    padding: 0 1rem;
+  list-style: none;
+  padding: 0 1rem;
 `;
 
 const NavLink = styled.a`
@@ -52,120 +52,126 @@ const NavLink = styled.a`
 `;
 
 const MyButton = styled(Button)`
-    border-radius: 0.5rem;
+  border-radius: 0.5rem;
 `;
 
 const Svg = styled.svg`
-    color: #2864f0;
+  color: #2864f0;
 `;
 
 const MenuItems = () => {
-    return (
-        <NavList>
-            <li><NavLink href="https://corp.freee.co.jp/company/">会社概要</NavLink></li>
-            <li><NavLink href="https://corp.freee.co.jp/mission/">ミッション</NavLink></li>
-            <li><NavLink href="https://corp.freee.co.jp/news/">ニュース</NavLink></li>
-        </NavList>
-    );
+  return (
+    <NavList>
+      <li>
+        <NavLink href="https://corp.freee.co.jp/company/">会社概要</NavLink>
+      </li>
+      <li>
+        <NavLink href="https://corp.freee.co.jp/mission/">ミッション</NavLink>
+      </li>
+      <li>
+        <NavLink href="https://corp.freee.co.jp/news/">ニュース</NavLink>
+      </li>
+    </NavList>
+  );
 };
 
 const HamburgerMenu: React.FC = () => {
-    // 開閉状態を管理するステート
-    const [isExpanded, setExpanded] = useState(false);
-    // 完全に閉じた状態を管理するステート
-    const [isHidden, setHidden] = useState(true);
+  // 開閉状態を管理するステート
+  const [isExpanded, setExpanded] = useState(false);
+  // 完全に閉じた状態を管理するステート
+  const [isHidden, setHidden] = useState(true);
 
-    const handleClick = () => {
-        // クリックした直後は開閉いずれの状態であってもhidden属性の値をfalseにする
-        setHidden(false);
-        // クリックしたときに開閉状態を切り替える
-        // アニメーションがスムーズに実行されるようにするため、hidden属性が反映されてからsetExpandedする
-        setTimeout(() => {
-            setExpanded(!isExpanded);
-        }, 0);
-    };
+  const handleClick = () => {
+    // クリックした直後は開閉いずれの状態であってもhidden属性の値をfalseにする
+    setHidden(false);
+    // クリックしたときに開閉状態を切り替える
+    // アニメーションがスムーズに実行されるようにするため、hidden属性が反映されてからsetExpandedする
+    setTimeout(() => {
+      setExpanded(!isExpanded);
+    }, 0);
+  };
 
-    const handleTransitionEnd = () => {
-        // アニメーションが終了しメニューが閉じたとき（isExpandedがfalseのとき）、hidden属性の値をtrueにする
-        !isExpanded && setHidden(true);
-    };
+  const handleTransitionEnd = () => {
+    // アニメーションが終了しメニューが閉じたとき（isExpandedがfalseのとき）、hidden属性の値をtrueにする
+    !isExpanded && setHidden(true);
+  };
 
-    return (
-        <HamburgerStyle>
-            <MyButton
-                type="button"
-                className="button"
-                onClick={handleClick}
-                aria-expanded={isExpanded}
-                aria-controls="hamburgermenu"
-                aria-haspopup="menu"
-            >
-                <Svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="48"
-                    viewBox="0 96 960 960"
-                    width="48"
-                    role="img"
-                    aria-label="目次"
-                >
-                    <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" fill="currentColor" />
-                </Svg>
-            </MyButton>
-            <div
-                className={`hamburgermenu${isExpanded ? ' expanded' : ''}`}
-                onTransitionEnd={handleTransitionEnd}
-                hidden={isHidden}
-                id="hamburgermenu"
-                aria-label="目次"
-            >
-                <MenuItems />
-            </div>
-        </HamburgerStyle>
-    );
+  return (
+    <HamburgerStyle>
+      <MyButton
+        type="button"
+        className="button"
+        onClick={handleClick}
+        aria-expanded={isExpanded}
+        aria-controls="hamburgermenu"
+        aria-haspopup="menu"
+      >
+        <Svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 96 960 960"
+          width="48"
+          role="img"
+          aria-label="目次"
+        >
+          <path
+            d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"
+            fill="currentColor"
+          />
+        </Svg>
+      </MyButton>
+      <div
+        className={`hamburgermenu${isExpanded ? ' expanded' : ''}`}
+        onTransitionEnd={handleTransitionEnd}
+        hidden={isHidden}
+        id="hamburgermenu"
+        aria-label="目次"
+      >
+        <MenuItems />
+      </div>
+    </HamburgerStyle>
+  );
 };
 
 const BadHamburgerMenu: React.FC = () => {
-    // 開閉状態を管理するステート
-    const [isExpanded, setExpanded] = useState(false);
+  // 開閉状態を管理するステート
+  const [isExpanded, setExpanded] = useState(false);
 
-    const handleClick = () => {
-        // クリックしたときに開閉状態を切り替える
-        setExpanded(!isExpanded);
-    };
+  const handleClick = () => {
+    // クリックしたときに開閉状態を切り替える
+    setExpanded(!isExpanded);
+  };
 
-    return (
-        <HamburgerStyle>
-            <MyButton
-                type="button"
-                className="button"
-                onClick={handleClick}
-            >
-                <Svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="48"
-                    viewBox="0 96 960 960"
-                    width="48"
-                >
-                    <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" fill="currentColor" />
-                </Svg>
-            </MyButton>
-            <div
-                className={`hamburgermenu${isExpanded ? ' expanded' : ''}`}
-            >
-                <MenuItems />
-            </div>
-        </HamburgerStyle>
-    );
+  return (
+    <HamburgerStyle>
+      <MyButton type="button" className="button" onClick={handleClick}>
+        <Svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 96 960 960"
+          width="48"
+        >
+          <path
+            d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"
+            fill="currentColor"
+          />
+        </Svg>
+      </MyButton>
+      <div className={`hamburgermenu${isExpanded ? ' expanded' : ''}`}>
+        <MenuItems />
+      </div>
+    </HamburgerStyle>
+  );
 };
 
 export const GoodExpandableButton = (): JSX.Element => (
-    <ExampleContainer>
-        <HamburgerMenu />
-    </ExampleContainer>
+  <ExampleContainer>
+    <HamburgerMenu />
+  </ExampleContainer>
 );
 
 export const BadExpandableButton = (): JSX.Element => (
-    <ExampleContainer>
-        <BadHamburgerMenu />
-    </ExampleContainer>
+  <ExampleContainer>
+    <BadHamburgerMenu />
+  </ExampleContainer>
 );
